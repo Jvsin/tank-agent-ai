4ce43cd0029d4da5a8d8ec728fb8d963a36f07bf
Lecrut <filip.lecrut12@gmail.com>
2026-02-12 19:53:14 +0100

    cleaning

--- 03_FRAKCJA_AGENTOW/training/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Training Package
-Zawiera moduły do treningu agenta algorytmem genetycznym.
-"""
-
-from .genetic_algorithm import GeneticAlgorithm
-from .fitness_evaluator import FitnessEvaluator
-
-__all__ = ['GeneticAlgorithm', 'FitnessEvaluator']
